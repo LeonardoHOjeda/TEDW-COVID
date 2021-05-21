9518d278f4a8b164dc751b12514aa56619c87fd9
Leonardo Hernandez Ojeda <leonardohernandezojeda@Leonardos-iMac.local>
2021-05-21 18:08:51 -0500

    Se hace el apartado de Login con validacion

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <b-navbar toggleable="md" type="dark" variant="info">
+      <b-container>
+        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+          <b-navbar-brand :to="{name: 'Home'}">COVID-19 APP</b-navbar-brand>
+          <b-collapse is-nav id="nav_collapse">
+            <b-navbar-nav>
+              <b-nav-item :to="{name: 'Home'}" active-class="active" exact>Home</b-nav-item>
+              <b-nav-item :to="{name: 'About'}" active-class="active">About</b-nav-item>
+              <b-nav-item :to="{name: 'Login'}" active-class="active">Login</b-nav-item>
+            </b-navbar-nav>
+          </b-collapse>
+      </b-container>
+    </b-navbar>
+    <b-container>
+      <router-view/>
+    </b-container>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="mt-5">
+    <h1>Login</h1>
+    <form @submit.prevent="submit">
+      <!-- Correo electronico -->
+      <div class="mb-3">
+        <label for="emailInstitute" class="form-label">Correo institucional</label>
+        <input 
+          type="email" 
+          placeholder="Ingresa tu correo institucional" 
+          class="form-control" v-model.lazy="$v.email.$model" 
+          :class="{'is-invalid': $v.email.$error, 'is-valid': $v.email.required}"
+          id="emailInstitute"
+          />
+        <p class="text-danger" v-if="!$v.email.email">Este email es incorrecto</p>
+        <p class="text-danger" v-if="!$v.email.required">Campo requerido</p>
+        <!-- <p>{{$v.email}}</p> -->
+      </div>
+
+      <!-- Constrasena -->
+      <label for="emailInstitute" class="form-label">Password</label>
+      <input type="password" placeholder="Ingresa Contrasena" class="form-control" v-model.lazy="$v.password.$model" :class="{'is-invalid': $v.password.$error, 'is-valid': $v.password.required}">
+      <p class="text-danger" v-if="!$v.password.minLength">Minimo 6 caracteres</p>
+      <p class="text-danger" v-if="!$v.password.required">Campo requerido</p>
+      <!-- <p>{{$v.password}}</p> -->
+
+      <b-button block variant="primary" type="submit" :disabled="$v.$invalid">Login</b-button>
+      <!-- <p>{{$v.$invalid}}</p> -->
+    </form>
+  </div>
+</template>
+
+<script>
+import { required, email, minLength} from 'vuelidate/lib/validators'
+export default {
+  name: 'Login',
+    data() {
+      return {
+        email: '',
+        password: ''
+      }
+    },
+    validations: {
+      email:{required, email},
+      password: {
+        required,
+        minLength: minLength(6)
+      }
+    },
+    methods: {
+      submit(){
+        console.log('Submit!');
+        this.$v.$touch()
+        if(this.$v.$invalid){
+          console.log('Se genero un error');
+        }else{
+          console.log('Todos los campos correctos');
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
