c29d8fffadc60c3b8b315c891b494087bcd81249
Leonardo <perros_rebeldes@hotmail.com>
2021-06-02 00:18:36 -0500

    Agrega la vista de imprimir recetas

---
+++ src/components/Titulos.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="mb-4">
+    <h1 class="text-center">{{titulo}}</h1>
+    <h3 class="font-weight-light text-center">{{subtitulo}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Titulos',
+  props: {
+    titulo: {
+      type: String,
+      default: 'Titulo h1'
+    },
+    subtitulo: {
+      type: String,
+      default: 'Subtitulo h3'
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/students/PrintRecipe.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <Titulos 
+      titulo="Historial de recetas"
+      subtitulo="En esta seccion podras imprimir las recetas medicas de las consultas que ya fueron atendidas"
+      />
+      <table class="table table-hover">
+        <thead class="thead-light">
+          <tr>
+            <th scope="col">ID CITA</th>
+            <th scope="col">Last</th>
+            <th scope="col">DESCARGAR RECETA</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Otto</td>
+            <td><a href="">Descargar </a><i class="far fa-file-pdf"></i></td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Thornton</td>
+            <td><a href="">Descargar </a><i class="far fa-file-pdf"></i></td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>the Bird</td>
+            <td><a href="">Descargar </a><i class="far fa-file-pdf"></i></td>
+          </tr>
+        </tbody>
+      </table>
+  </div>
+</template>
+
+<script>
+import Titulos from '../../components/Titulos'
+export default {
+  components: {
+    Titulos
+  }
+}
+</script>
+
+<style>
+
+</style>
