0aa1ee3a4dad348eefbe42e30c3b5d79d3a5e821
Fernando Acosta <Fernando Acosta>
2021-06-27 19:10:40 -0500

    Cambio a los titulos

--- src/components/Titulos.vue
+++ src/components/Titulos.vue
@@ -1,26 +1,29 @@
 <template>
-  <div class="mb-4">
-    <h1 class="text-center">{{titulo}}</h1>
-    <h3 class="font-weight-light text-center">{{subtitulo}}</h3>
+  <div class="mb-4 container-fluid">
+    <div class="card p-1 shadow-sm">
+      <div class="card-body">
+        <h2 class="h3">{{titulo}}</h2>
+        <h6 class="font-weight-light">{{subtitulo}}</h6>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Titulos',
-  props: {
-    titulo: {
-      type: String,
-      default: ''
+  export default {
+    name: 'Titulos',
+    props: {
+      titulo: {
+        type: String,
+        default: '',
+      },
+      subtitulo: {
+        type: String,
+        default: '',
+      },
     },
-    subtitulo: {
-      type: String,
-      default: ''
-    },
-  }
-}
+  };
 </script>
 
 <style>
-
 </style>
